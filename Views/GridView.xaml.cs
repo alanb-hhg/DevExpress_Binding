6a047eecb174fbe0e3bd6aac1b3294a2a2b8c312
alanb-hhg <alanb@hhgroup.co.za>
2024-05-22 19:30:20 +0200

    Cleanup

--- Views/GridView.xaml.cs
+++ Views/GridView.xaml.cs
@@ -1,22 +1,9 @@
-﻿using DevExpress.Xpf.Grid;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Example.Views
 {
@@ -24,7 +11,7 @@ namespace Example.Views
     public partial class GridView : UserControl, INotifyPropertyChanged
     {
         static string path = @"..\..\..\Resources\Contacts.xml";
-        //public int countRows { get { return Counter; } }
+
         public int _counter;
 
         public int Counter
@@ -37,16 +24,13 @@ namespace Example.Views
             }
         }
 
-        //        CountModel countModel = new CountModel();
+
         public GridView()
         {
             InitializeComponent();
             grid.ItemsSource = GetDataFromXML();
             txtCounter.DataContext = Counter;
         }
-
-        //public class CountModel : INotifyPropertyChanged
-
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -61,8 +45,7 @@ namespace Example.Views
             ds.ReadXml(path);
             return ds.Tables[0];
         }
-
-        private void table_PropertyChanged(object sender, PropertyChangedEventArgs e)
+                private void table_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             Counter = grid.VisibleRowCount;
         }
